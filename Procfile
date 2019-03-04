1343ac65fdb64b0daca8ae388afc6009939e1971
Ross Alexandra <rossandus@hotmail.com>
2019-03-04 12:36:57 -0800

    Updated repo to no longer use django-heroku
     - Should be using containers now.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app.wsgi
+web: docker-compose up
--- app/app/settings.py
+++ app/app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-import django_heroku
+# import django_heroku
 
 # Get the secret key
 try:
@@ -59,7 +59,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "app.urls"
@@ -128,10 +127,5 @@ STATIC_URL = "/static/"
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 # Must be at end of settings.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
