c2690c3174b6c069f546e04f0a22d42615c134ac
Ross Alexandra <rossandus@hotmail.com>
2019-03-05 16:31:58 -0800

    GH-23 - multiple changes
     - Updated the settings structure for dev
       vs prod.
    
     - Setup the settings for database in prod.

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -34,11 +34,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
 ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 
@@ -80,18 +76,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "app.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
--- app/start_server.sh
+++ app/start_server.sh
@@ -1,1 +1,1 @@
-python app/manage.py runserver "0.0.0.0:$PORT"
+python app/manage.py runserver "0.0.0.0:$PORT" --settings=app.settings.prod
