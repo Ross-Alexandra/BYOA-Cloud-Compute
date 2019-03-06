99d3d27e60dc99e95e7a2c2557e50bdf21c55ce5
eetar1 <ekwan14@gmail.com>
2019-03-06 13:42:31 -0800

    first unittest

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,7 +3,16 @@ from django.test import Client, TestCase
 from django.urls import reverse
 
 
-def test_getLogin(self):
-    client = Client()
-    response = client.get("http://127.0.0.1:8000/accounts/login/")
-    self.assertEqual(response.status_code, 200)
+class test_Accounts(TestCase):
+    def setUp(self):
+        # Setup run before every test method.
+        pass
+
+    def tearDown(self):
+        # Clean up run after every test method.
+        pass
+
+    def test_getLogin(self):
+        client = Client()
+        response = client.get("http://127.0.0.1:8000/accounts/login/")
+        self.assertEqual(response.status_code, 200)
