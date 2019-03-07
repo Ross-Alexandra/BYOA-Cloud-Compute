9d67625cfb2e0d5f8b0bc5f8ed86ac0bbdb78595
eetar1 <ekwan14@gmail.com>
2019-03-06 21:34:00 -0800

    first unittest

---
+++ app/app/tests.py
@@ -0,0 +1,10 @@
+import pytest
+from django.test import Client, TestCase
+from django.urls import reverse
+
+
+class test_Accounts(TestCase):
+    def test_getLogin(self):
+        client = Client()
+        response = client.get("http://127.0.0.1:8000/accounts/login/")
+        self.assertEqual(response.status_code, 200)
