aeea9e945d066a9eca83e8fb91d2af0268a83e77
Joshua McIntosh <jmcintosh4545@gmail.com>
2019-03-12 12:17:12 -0700

    GH-14 Changes to unit tests

--- app/tests/test_urls.py
+++ app/tests/test_urls.py
@@ -3,19 +3,25 @@ from django.urls import reverse
 
 
 class test_accounts_page(TestCase):
+    def setUp(self):
+        self.client = Client()
+
     def test_getLogin(self):
-        client = Client()
-        response = client.get("http://127.0.0.1:8000/accounts/login/")
+        response = self.client.get("http://127.0.0.1:8000/accounts/login/")
         assert response.status_code == 200
 
     def test_landing_redirect(self):
-        client = Client()
-        response = client.get("http://127.0.0.1:8000", follow=True)
+        response = self.client.get("http://127.0.0.1:8000", follow=True)
         SimpleTestCase().assertRedirects(response, "/accounts/login/?next=/")
+
+    def test_signup_button(self):
+        response = self.client.get("http://127.0.0.1:8000/accounts/login/")
 
 
 class test_signup_page(TestCase):
+    def setUP(self):
+        self.client = Client()
+
     def test_getSignUp(self):
-        client = Client()
-        response = client.get("http://127.0.0.1:8000/accounts/signup/")
+        response = self.client.get("http://127.0.0.1:8000/accounts/signup/")
         assert response.status_code == 200
