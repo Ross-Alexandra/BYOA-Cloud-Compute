080e4acdc41971a35b390657799d83934f7633e1
Joshua McIntosh <jmcintosh4545@gmail.com>
2019-03-14 00:52:41 -0700

    GH-14 Fixing and reorganizing after GH-23

--- ccf/app/views.py
+++ ccf/app/views.py
@@ -1,12 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import generic
 
 
+@login_required
 # Create your views here.
 def homepage_view(request):
-    return redirect("/accounts/login/")
+    return render(request, "tmp_home.html")
 
 
 class SignUp(generic.CreateView):
