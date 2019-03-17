42c38d29b78bfbb0b673d7d2a4882d13cba8f8da
Joshua McIntosh <jmcintosh4545@gmail.com>
2019-03-17 13:57:00 -0700

    GH-14 Changes as per code review and deleted leftover files during dev.

--- ccf/app/views.py
+++ ccf/app/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
