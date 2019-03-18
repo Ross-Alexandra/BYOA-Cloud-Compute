9facdcb3340375fcf3b22ee861c24c46d6f28dd6
Joshua McIntosh <jmcintosh4545@gmail.com>
2019-03-17 20:31:24 -0700

    GH-14 Added comments to views.py as per review.

--- ccf/app/views.py
+++ ccf/app/views.py
@@ -6,12 +6,21 @@ from django.views import generic
 
 
 @login_required
-# Create your views here.
 def homepage_view(request):
+    """Homepage to Login Redirect
+
+    Returns to the user the homepage template if they are logged in, otherwise redirects them
+    to the login page.
+    """
     return render(request, "tmp_home.html")
 
 
 class SignUp(generic.CreateView):
+    """Sign-up Page
+
+    Returns to the user the sign-up template.
+    """
+
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
